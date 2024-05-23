f79ed83e3979c3648055598d9ed62887868ece52
teodora stoykowa <t.stoykowa@gmail.com>
2024-05-23 17:08:24 +0300

    home properties

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import Footer from "@/components/Footer";
+import HomePropeties from "@/components/HomePropeties";
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomePropeties />
       <Footer />
     </>
   );
