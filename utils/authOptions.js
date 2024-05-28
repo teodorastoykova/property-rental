98bc059d43299585e310c5189402712a8bfc78db
teodora stoykowa <t.stoykowa@gmail.com>
2024-05-28 14:52:24 +0300

    authprovider

---
+++ utils/authOptions.js
@@ -0,0 +1,48 @@
+import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    //on success
+    async signIn({ profile }) {
+      await connectDB();
+
+      const userExists = await User.findOne({ email: profile.email });
+
+      if (!userExists) {
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username: username,
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    },
+
+    //modifies the session object
+    async session({ session }) {
+      const user = await User.findOne({ email: session.user.email });
+
+      session.user.id = user._id.toString();
+      
+      return session;
+    },
+  },
+};
