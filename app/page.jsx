336359fd345718ad07c688249e4ba0e733160514
teodora stoykowa <t.stoykowa@gmail.com>
2024-05-29 18:45:49 +0300

    footer moved to the main layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-import Footer from "@/components/Footer";
 import HomePropeties from "@/components/HomePropeties";
 const HomePage = () => {
   return (
@@ -9,7 +8,6 @@ const HomePage = () => {
       <Hero />
       <InfoBoxes />
       <HomePropeties />
-      <Footer />
     </>
   );
 };
