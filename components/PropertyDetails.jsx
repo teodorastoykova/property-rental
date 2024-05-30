689cde0b89c697061c82ff33beb7c1877d644981
teodora stoykowa <t.stoykowa@gmail.com>
2024-05-30 16:59:53 +0300

    property details modified to include the map component

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -7,6 +7,7 @@ import {
   FaCheck,
   FaMapMarker,
 } from "react-icons/fa";
+import PropertyMap from "./PropertyMap";
 
 const PropertyDetails = ({ property }) => {
   return (
@@ -92,7 +93,9 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <div id="map">
+          <PropertyMap property={property}/>
+        </div>
       </div>
     </main>
   );
