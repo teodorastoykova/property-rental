169a073bf05e39ea3fd27d9035bab2ab62624723
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-03 16:35:41 +0300

    new messages page

---
+++ app/messages/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Messages from "@/components/Messages";
+
+const MessagesPage = () => {
+  return (
+    <Messages ></Messages>
+  );
+};
+
+export default MessagesPage;
