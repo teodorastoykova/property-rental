3a4d90150345a14c7becb2b9132ac64f5d632362
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-03 16:34:24 +0300

    messages component

---
+++ components/Messages.jsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
+import Message from "@/components/Message";
+
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await fetch("/api/messages");
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setMessages(data);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getMessages();
+  }, []);
+
+  return isLoading ? (
+    <Spinner loading={isLoading} />
+  ) : (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+          <div className="space-y-4"></div>
+          {messages.length == 0 ? (
+            <p>You have no messages</p>
+          ) : (
+            messages.map((message) => (
+              <Message key={message._id} message={message} />
+            ))
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Messages;
