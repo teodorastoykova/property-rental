03066a54e8b1c8fbfcf9a0ae791ce243245d611e
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-04 15:56:36 +0300

    featured properties component & route

--- utils/request.js
+++ utils/request.js
@@ -1,13 +1,13 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // fetch all properties
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     if (!apiDomain) {
       return [];
     }
 
-    const res = await fetch(`${apiDomain}/properties`, {
+    const res = await fetch(`${apiDomain}/properties${showFeatured ? '/featured' : ''}`, {
       cache: "no-store",
     });
 
