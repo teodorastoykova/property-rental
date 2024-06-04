1ee5ebf52ae41a16e2bf08448f5dd084c25cea1f
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-04 16:28:08 +0300

    -

--- app/api/properties/search/route.js
+++ app/api/properties/search/route.js
@@ -1,7 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
-export const dynamic = "force-dynamic";
 //get /api/properties/search
 
 export const GET = async (request) => {
