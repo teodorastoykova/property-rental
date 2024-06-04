18f88f8ae6748f14f7c6b56dd9a27165d3285ba5
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-04 17:24:22 +0300

    -

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -2,19 +2,15 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 
 export const getSessionUser = async () => {
-  try {
-    const session = await getServerSession(authOptions);
-
-    if (!session || !session.user) {
-      return null;
-    }
-
-    return {
-      user: session.user,
-      userId: session.user.id,
-    };
-  } catch (error) {
-    console.error(error);
+  
+  const session = await getServerSession(authOptions);
+ 
+  if (!session || !session.user) {
     return null;
   }
+ 
+  return {
+    user: session.user,
+    userId: session.user.id,
+  };
 };
