75d7929c7a34898861b8832f038a47301219a108
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-04 21:42:01 +0300

    added /api

--- middleware.js
+++ middleware.js
@@ -1,5 +1,11 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/properties/add", "/profile", "/properties/saved", "/messages"],
+  matcher: [
+    "/properties/add",
+    "/profile",
+    "/properties/saved",
+    "/messages",
+    "/api/*",
+  ],
 };
