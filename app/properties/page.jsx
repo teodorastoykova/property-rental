c0aa762b22c1c75def87ec4c559f07a0b914ab07
teodora stoykowa <t.stoykowa@gmail.com>
2024-06-05 13:36:35 +0300

    rever propertiespage

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import PropertyCard from "@/components/PropertyCard";
 import { fetchProperties } from "@/utils/request";
 import PropertySearchForm from "@/components/PropertySearchForm";
-import Link from "next/link";
-import { FaArrowAltCircleLeft } from "react-icons/fa";
-import Pagination from "@/components/Pagination";
-const PropertiesPage = async () => {
-  const data = await fetchProperties();
+import Properties from "@/components/Properties";
 
-  const properties = data.properties
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+const PropertiesPage = async () => {
 
   return (
     <>
@@ -18,27 +12,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-      <section className="px-4 py-6">
-        <h1 className="text-2xl mb-4 text-center">All Properties</h1>
-        <Link
-          href="/properties"
-          className="flex items-center text-blue-500 hover:underline mb-4"
-        >
-          <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back to Properties
-        </Link>
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-            
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
